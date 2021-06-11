274b803caea998fe5ecdc7bc5b83e4671de3c829
macrom1cro <macromicro37@gmail.com>
2021-06-11 20:45:08 +0300

    3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -12,10 +12,21 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
 
     }
 
-    Connection connection = getConnection();
+    private Connection connection = getConnection();
 
     public void createUsersTable() {
+        String sql = "create table Users2 (id BIGINT not null auto_increment, " +
+                "name VARCHAR (30) not null, " +
+                "lastname VARCHAR (30) not null, " +
+                "age TINYINT not null, " +
+                "PRIMARY KEY (id))";
 
+        try (Statement statement = connection.createStatement()){
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("no createUsersTable");
+        }
     }
 
     public void dropUsersTable() {
@@ -29,24 +40,34 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
             preparedStatement.setString(3, lastName);
             preparedStatement.setByte(4, age);
 
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-            System.out.println("no add User");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("no saveUser");
         }
     }
 
     public void removeUserById(long id) {
-
+        String sql = "";
     }
 
     public List<User> getAllUsers() {
-        List<User> userList= new ArrayList<>();
+        List<User> userList = new ArrayList<>();
         String sql = "SELECT ID, NAME, LASTNAME, AGE FROM USERS";
-        try (Statement statement = connection.createStatement()){
+        try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastname"));
+                user.setAge(resultSet.getByte("age"));
 
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+                userList.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("no getAllUsers");
         }
         return userList;
     }
