beb7c5f4571da8bb3e951ae9399602daaaa6bbae
macrom1cro <macromicro37@gmail.com>
2021-06-13 21:43:14 +0300

    Доделал

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,27 @@
 package jm.task.core.jdbc;
 
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ivan","Ivanov", (byte) 20);
+        userService.saveUser("Sam","Qwerty", (byte) 25);
+        userService.saveUser("Alex","Bot", (byte) 30);
+        userService.saveUser("Tom","Cat", (byte) 35);
+        List<User> userList = userService.getAllUsers();
+        for (User user : userList) {
+            System.out.println(user.toString());
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,32 +1,16 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl extends Util implements UserDao {
+public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
 
     }
 
-    private Connection connection = getConnection();
-
     public void createUsersTable() {
-        String sql = "create table Users2 (id BIGINT not null auto_increment, " +
-                "name VARCHAR (30) not null, " +
-                "lastname VARCHAR (30) not null, " +
-                "age TINYINT not null, " +
-                "PRIMARY KEY (id))";
 
-        try (Statement statement = connection.createStatement()){
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("no createUsersTable");
-        }
     }
 
     public void dropUsersTable() {
@@ -34,42 +18,15 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO USERS (NAME, LASTNAME, AGE)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setString(2, name);
-            preparedStatement.setString(3, lastName);
-            preparedStatement.setByte(4, age);
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("no saveUser");
-        }
     }
 
     public void removeUserById(long id) {
-        String sql = "";
+
     }
 
     public List<User> getAllUsers() {
-        List<User> userList = new ArrayList<>();
-        String sql = "SELECT ID, NAME, LASTNAME, AGE FROM USERS";
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastname"));
-                user.setAge(resultSet.getByte("age"));
-
-                userList.add(user);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("no getAllUsers");
-        }
-        return userList;
+        return null;
     }
 
     public void cleanUsersTable() {
